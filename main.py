6e551ba66291c12c0a29590b569218e7c8addb1b
Tudor <branettudor22@gmail.com>
2022-12-07 22:06:51 +0200

    game done

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ import keyboard
 x_pos = 4
 y_pos = 0
 score = 0
+safe_pos_string = "Safe positions: "
 
 game_grid = [
     [0, 0, 0, 0, 0],
@@ -40,15 +41,22 @@ block_message = [
 ]
 
 read_expr = Expression.fromstring
-p1 = read_expr('adjacent(x1, y1, x2, y2) <-> (x1=x2+1 & y1=y2) | (x2=x1+1 & y1=y2) | (x1=x2 & y1=y2+1) | (x1=x2 & y2=y1+1)')
-p2 = read_expr('all x1 all y1 (heat(x1, y1) <-> exists x2 y2(lava(x2, y2) & adjacent(x1, y1, x2, y2)))')
-p3 = read_expr('all x1 all y1 (gas_nearby(x1, y1) <-> exists x2 y2(gas(x2, y2) & adjacent(x1, y1, x2, y2)))')
-p4 = read_expr('all x1 all y1 (empty(x1, y1) <-> -exists x2 y2((lava(x2, y2) | gas(x2, y2)) & adjacent(x1, y1, x2, y2)))')
-p5 = read_expr('all x1 all y1 (heat(x1, y1) -> -gas(x1, y1) & -empty(x1, y1))')
-p6 = read_expr('all x1 all y1 (gas(x1, y1) -> -heat(x1, y1) & -empty(x1, y1))')
-p7 = read_expr('all x1 all y1 (empty(x1, y1) -> -gas(x1, y1) & -heat(x1, y1))')
-rules = [p1, p2, p3, p4, p5, p6, p7]
+rules = []
 assumptions = []
+
+
+def get_rules_from_file(file_name):
+    rules = []
+    with open(file_name, "r") as f:
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            if not line.isspace():
+                rules.append(read_expr(line))
+
+    return rules
+
 
 def generate_map():
     # choose gold position
@@ -60,7 +68,7 @@ def generate_map():
         for column in range(5):
             random_nr = random.randint(1, 10)
             if block_type[row][column] == 0 and row != 4 and column != 0:
-                if random_nr % 4 == 0 and lava < 4:
+                if random_nr % 4 == 0 and lava < 5:
                     block_type[row][column] = 2
                     lava += 1
                 elif random_nr == 9:
@@ -79,16 +87,19 @@ def reset_game():
     assumptions.clear()
     assumptions.extend(rules)
 
-    global x_pos, y_pos, score
+    global x_pos, y_pos, score, safe_pos_string
     score = 0
     x_pos = 4
     y_pos = 0
     score_lbl.configure(text="Score: " + str(score))
     game_grid[x_pos][y_pos].configure(borderwidth=3, image=msg_to_pic(block_message[x_pos][y_pos]))
 
+    safe_pos_lbl.configure(text=safe_pos_string)
+    safe_pos_string = "Safe positions: "
+
 
 def key_pressed(event, window, score_lbl):
-    global x_pos, y_pos, score
+    global x_pos, y_pos, score, safe_pos_string
     if event.char == "w":
         game_grid[x_pos][y_pos].configure(borderwidth=1)
         if x_pos > 0:
@@ -105,33 +116,18 @@ def key_pressed(event, window, score_lbl):
         game_grid[x_pos][y_pos].configure(borderwidth=1)
         if y_pos > 0:
             y_pos = y_pos - 1
-    elif event.char == 'c':
-        g1 = read_expr('-gas(' + str(x_pos - 1) + ',' + str(y_pos) + ') & -lava(' + str(x_pos - 1) + ',' + str(
-            y_pos) + ')')  # UP is safe
-        g2 = read_expr('-gas(' + str(x_pos + 1) + ',' + str(y_pos) + ') & -lava(' + str(x_pos + 1) + ',' + str(
-            y_pos) + ')')  # DOWN is safe
-        g3 = read_expr('-gas(' + str(x_pos) + ',' + str(y_pos - 1) + ') & -lava(' + str(x_pos) + ',' + str(
-            y_pos - 1) + ')')  # LEFT is safe
-        g4 = read_expr('-gas(' + str(x_pos) + ',' + str(y_pos + 1) + ') & -lava(' + str(x_pos) + ',' + str(
-            y_pos + 1) + ')')  # RIGHT is safe
-
-        print("UP: " + str(Prover9().prove(g1, assumptions)))
-        print("DOWN: " + str(Prover9().prove(g2, assumptions)))
-        print("LEFT: " + str(Prover9().prove(g3, assumptions)))
-        print("RIGHT: " + str(Prover9().prove(g4, assumptions)))
-        print("\n")
 
     my_image = msg_to_pic(block_message[x_pos][y_pos])
     game_grid[x_pos][y_pos].configure(borderwidth=3, image=my_image)
     if my_image == gas_msg_img:
-        assumptions.append(read_expr('gas_nearby('+str(x_pos)+','+str(y_pos)+')'))
+        assumptions.append(read_expr('GN' + str(x_pos) + str(y_pos)))
     elif my_image == heat_msg_img:
-        assumptions.append(read_expr('heat(' + str(x_pos) + ',' + str(y_pos) + ')'))
+        assumptions.append(read_expr('H' + str(x_pos) + str(y_pos)))
     elif my_image == heat_gas_msg_img:
-        assumptions.append(read_expr('heat(' + str(x_pos) + ',' + str(y_pos) + ')'))
-        assumptions.append(read_expr('gas_nearby(' + str(x_pos) + ',' + str(y_pos) + ')'))
+        assumptions.append(read_expr('H' + str(x_pos) + str(y_pos)))
+        assumptions.append(read_expr('GN' + str(x_pos) + str(y_pos)))
     elif my_image == empty_msg_img:
-        assumptions.append(read_expr('empty(' + str(x_pos) + ',' + str(y_pos) + ')'))
+        assumptions.append(read_expr('E' + str(x_pos) + str(y_pos)))
 
     if block_type[x_pos][y_pos] == 2:
         resp = messagebox.askquestion('Game Over', 'Do you want to play again?')
@@ -142,7 +138,7 @@ def key_pressed(event, window, score_lbl):
     elif block_type[x_pos][y_pos] == 1:
         score += 500
         score_lbl.configure(text="Score: " + str(score))
-        resp = messagebox.askquestion('You found the gold! Score: ' + str(score), 'Do you want to play again?')
+        resp = messagebox.askquestion('You found the gold! Congratulations!', 'Do you want to play again?')
         if resp == 'no':
             window.after(1000, lambda: window.destroy())
         else:
@@ -154,11 +150,40 @@ def key_pressed(event, window, score_lbl):
         score -= 5
         score_lbl.configure(text="Score: " + str(score))
 
+    x_1 = x_pos - 1
+    x_1_plus = x_pos + 1
+    y_1 = y_pos - 1
+    y_1_plus = y_pos + 1
+
+    if x_1 >= 0:
+        g1 = read_expr('-G' + str(x_1) + str(y_pos) + ' & -L' + str(x_1) + str(
+            y_pos))  # UP is safe
+        if Prover9().prove(g1, assumptions):
+            safe_pos_string += "UP "
+    if x_1_plus <= 4:
+        g2 = read_expr('-G' + str(x_1_plus) + str(y_pos) + ' & -L' + str(x_1_plus) + str(
+            y_pos))  # DOWN is safe
+        if Prover9().prove(g2, assumptions):
+            safe_pos_string += "DOWN "
+    if y_1_plus <= 4:
+        g3 = read_expr('-G' + str(x_pos) + str(y_1_plus) + ' & -L' + str(x_pos) + str(
+            y_1_plus))  # RIGHT is safe
+        if Prover9().prove(g3, assumptions):
+            safe_pos_string += "RIGHT "
+    if y_1 >= 0:
+        g4 = read_expr('-G' + str(x_pos) + str(y_1) + ' & -L' + str(x_pos) + str(
+            y_1))  # LEFT is safe
+        if Prover9().prove(g4, assumptions):
+            safe_pos_string += "LEFT"
+
+    safe_pos_lbl.configure(text=safe_pos_string)
+    safe_pos_string = "Safe positions: "
+
 
 def create_start_window():
     window = Tk()
     window.title("Gold-digging game")
-    window.geometry("800x600")
+    window.geometry("800x650")
     window.resizable(False, False)
     window.configure(background='#84c497')
     window_title = Label(text="Dig for gold!", font=('consolas', 30), background='#84c497')
@@ -238,6 +263,7 @@ if __name__ == "__main__":
 
     generate_map()
     generate_block_messages()
+    rules = get_rules_from_file("rules.txt")
     assumptions.extend(rules)
 
     gold_img = ImageTk.PhotoImage(Image.open("gold.png").resize((150, 100), Image.Resampling.LANCZOS))
@@ -250,9 +276,36 @@ if __name__ == "__main__":
     empty_msg_img = ImageTk.PhotoImage(Image.open("empty_message.png").resize((150, 100), Image.Resampling.LANCZOS))
 
     game_grid[x_pos][y_pos].configure(borderwidth=3, image=msg_to_pic(block_message[x_pos][y_pos]))
-
-    score_lbl = Label(text="Score: 0", font=('consolas', 30), background='#84c497')
+    score_lbl = Label(text="Score: 0", font=('consolas', 30), background='#84c497', fg='#94091e')
     score_lbl.pack(side=BOTTOM)
+
+    x = 4
+    y = 0
+    my_image = msg_to_pic(block_message[x][y])
+    game_grid[x][y].configure(borderwidth=3, image=my_image)
+    if my_image == gas_msg_img:
+        assumptions.append(read_expr('GN' + str(x) + str(y)))
+    elif my_image == heat_msg_img:
+        assumptions.append(read_expr('H' + str(x) + str(y)))
+    elif my_image == heat_gas_msg_img:
+        assumptions.append(read_expr('H' + str(x) + str(y)))
+        assumptions.append(read_expr('GN' + str(x) + str(y)))
+    elif my_image == empty_msg_img:
+        assumptions.append(read_expr('E' + str(x) + str(y)))
+
+    goal_1 = read_expr('-G' + str(x-1) + str(y) + ' & -L' + str(x-1) + str(
+        y))  # UP is safe
+    if Prover9().prove(goal_1, assumptions):
+        safe_pos_string += "UP "
+
+    goal_2 = read_expr('-G' + str(x) + str(y+1) + ' & -L' + str(x) + str(
+        y+1))  # RIGHT is safe
+    if Prover9().prove(goal_2, assumptions):
+        safe_pos_string += "RIGHT "
+
+    safe_pos_lbl = Label(text=safe_pos_string, font=('consolas', 25), background='#84c497', fg='#5534eb')
+    safe_pos_string = "Safe positions: "
+    safe_pos_lbl.pack(side=BOTTOM)
 
     window.mainloop()
 
